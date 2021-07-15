17109b474039165be8706b01100dbc1f69f464ae
Hady Mashhour <63734405+HadyMash@users.noreply.github.com>
2021-07-15 14:35:38 +0400

    added firebase auth.

--- lib/main.dart
+++ lib/main.dart
@@ -9,63 +9,75 @@ void main() async {
 
   runApp(
     const MaterialApp(
-      home: Test(),
+      home: Home(),
     ),
   );
 }
 
-class Test extends StatefulWidget {
-  const Test({Key? key}) : super(key: key);
-
-  @override
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> with WidgetsBindingObserver {
-  @override
-  void initState() {
-    super.initState();
-    if (kIsWeb) {
-      window.addEventListener('visibilitychange', onVisibilityChange);
-    } else {
-      WidgetsBinding.instance!.addObserver(this);
-    }
-  }
-
-  @override
-  void dispose() {
-    if (kIsWeb) {
-      window.removeEventListener('visibilitychange', onVisibilityChange);
-    } else {
-      WidgetsBinding.instance!.removeObserver(this);
-    }
-    super.dispose();
-  }
-
-  void onVisibilityChange(Event e) {
-    String visibility = document.visibilityState;
-    if (visibility == 'visible') {
-      didChangeAppLifecycleState(AppLifecycleState.resumed);
-    } else if (visibility == 'hidden') {
-      didChangeAppLifecycleState(AppLifecycleState.paused);
-    }
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    print(state);
-  }
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Test'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text('Hello'),
-      ),
-    );
+    return Container();
   }
 }
+
+// class Test extends StatefulWidget {
+//   const Test({Key? key}) : super(key: key);
+
+//   @override
+//   _TestState createState() => _TestState();
+// }
+
+// class _TestState extends State<Test> with WidgetsBindingObserver {
+//   @override
+//   void initState() {
+//     super.initState();
+//     if (kIsWeb) {
+//       window.addEventListener('visibilitychange', onVisibilityChange);
+//     } else {
+//       WidgetsBinding.instance!.addObserver(this);
+//     }
+//   }
+
+//   @override
+//   void dispose() {
+//     if (kIsWeb) {
+//       window.removeEventListener('visibilitychange', onVisibilityChange);
+//     } else {
+//       WidgetsBinding.instance!.removeObserver(this);
+//     }
+//     super.dispose();
+//   }
+
+//   void onVisibilityChange(Event e) {
+//     String visibility = document.visibilityState;
+//     if (visibility == 'visible') {
+//       didChangeAppLifecycleState(AppLifecycleState.resumed);
+//     } else if (visibility == 'hidden') {
+//       didChangeAppLifecycleState(AppLifecycleState.paused);
+//     }
+//   }
+
+//   @override
+//   void didChangeAppLifecycleState(AppLifecycleState state) {
+//     print(state);
+//     setState(() => text = 'you cheated.');
+//   }
+
+//   String text = 'Hello';
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Test'),
+//         centerTitle: true,
+//       ),
+//       body: Center(
+//         child: Text(text),
+//       ),
+//     );
+//   }
+// }
