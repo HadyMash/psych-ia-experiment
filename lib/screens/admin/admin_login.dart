6d25dfb21b81dc66fa754a688fedbfae093394ea
HadyMash <63734405+HadyMash@users.noreply.github.com>
2021-08-25 00:02:50 +0400

    added autofill to admin auth screen

--- lib/screens/admin/admin_login.dart
+++ lib/screens/admin/admin_login.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/services.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:reading_experiment/screens/admin/admin_panel.dart';
 import 'package:reading_experiment/services/auth.dart';
@@ -92,119 +93,126 @@ class _AdminLoginState extends State<AdminLogin> {
               const SizedBox(height: 20),
               Form(
                 key: _formKey,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextFormField(
-                      focusNode: _emailFocusNode,
-                      controller: _emailController,
-                      onChanged: (val) => email = val,
-                      decoration: InputDecoration(
-                        hintText: 'example@ais.ae',
-                        labelText: 'email',
-                        labelStyle: TextStyle(
-                          color: _emailFocusNode.hasFocus
-                              ? Colors.blue
-                              : Colors.grey,
-                        ),
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(10, 15, 8, 20),
-                        border:
-                            CustomWidgetBorder(color: Colors.grey, width: 1.2),
-                        enabledBorder:
-                            CustomWidgetBorder(color: Colors.grey, width: 1.2),
-                        errorBorder: CustomWidgetBorder(
-                            color: Colors.red[300], width: 1.5),
-                        focusedErrorBorder: CustomWidgetBorder(
-                            color: Colors.red[300], width: 2.4),
-                        errorStyle: const TextStyle(fontSize: 14),
-                        focusedBorder:
-                            CustomWidgetBorder(color: Colors.blue, width: 2.2),
-                        prefixIcon: Icon(
-                          Icons.mail,
-                          color: _emailFocusNode.hasFocus
-                              ? Colors.blue
-                              : Colors.grey,
-                        ),
-                        suffixIcon: IconButton(
-                          icon: Icon(
-                            Icons.clear,
+                child: AutofillGroup(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        focusNode: _emailFocusNode,
+                        controller: _emailController,
+                        onChanged: (val) => email = val,
+                        autofillHints: const [
+                          AutofillHints.email,
+                          AutofillHints.username,
+                        ],
+                        decoration: InputDecoration(
+                          hintText: 'example@ais.ae',
+                          labelText: 'email',
+                          labelStyle: TextStyle(
                             color: _emailFocusNode.hasFocus
                                 ? Colors.blue
                                 : Colors.grey,
                           ),
-                          onPressed: () {
-                            _emailController.clear();
-                            email = '';
-                          },
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(10, 15, 8, 20),
+                          border: CustomWidgetBorder(
+                              color: Colors.grey, width: 1.2),
+                          enabledBorder: CustomWidgetBorder(
+                              color: Colors.grey, width: 1.2),
+                          errorBorder: CustomWidgetBorder(
+                              color: Colors.red[300], width: 1.5),
+                          focusedErrorBorder: CustomWidgetBorder(
+                              color: Colors.red[300], width: 2.4),
+                          errorStyle: const TextStyle(fontSize: 14),
+                          focusedBorder: CustomWidgetBorder(
+                              color: Colors.blue, width: 2.2),
+                          prefixIcon: Icon(
+                            Icons.mail,
+                            color: _emailFocusNode.hasFocus
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(
+                              Icons.clear,
+                              color: _emailFocusNode.hasFocus
+                                  ? Colors.blue
+                                  : Colors.grey,
+                            ),
+                            onPressed: () {
+                              _emailController.clear();
+                              email = '';
+                            },
+                          ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    TextFormField(
-                      focusNode: _passwordFocusNode,
-                      controller: _passwordController,
-                      onChanged: (val) => password = val,
-                      obscureText: _obscureText,
-                      decoration: InputDecoration(
-                        labelText: 'password',
-                        labelStyle: TextStyle(
-                          color: _passwordFocusNode.hasFocus
-                              ? Colors.blue
-                              : Colors.grey,
-                        ),
-                        contentPadding:
-                            const EdgeInsets.fromLTRB(10, 15, 8, 20),
-                        border:
-                            CustomWidgetBorder(color: Colors.grey, width: 1.2),
-                        enabledBorder:
-                            CustomWidgetBorder(color: Colors.grey, width: 1.2),
-                        errorBorder: CustomWidgetBorder(
-                            color: Colors.red[300], width: 1.5),
-                        focusedErrorBorder: CustomWidgetBorder(
-                            color: Colors.red[300], width: 2.4),
-                        errorStyle: const TextStyle(fontSize: 14),
-                        focusedBorder:
-                            CustomWidgetBorder(color: Colors.blue, width: 2.2),
-                        prefixIcon: Icon(
-                          Icons.lock_rounded,
-                          color: _passwordFocusNode.hasFocus
-                              ? Colors.blue
-                              : Colors.grey,
-                        ),
-                        suffixIcon: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            IconButton(
-                              icon: Icon(
-                                _obscureText
-                                    ? Icons.visibility_rounded
-                                    : Icons.visibility_off_rounded,
-                                color: _passwordFocusNode.hasFocus
-                                    ? Colors.blue
-                                    : Colors.grey,
+                      const SizedBox(height: 10),
+                      TextFormField(
+                        focusNode: _passwordFocusNode,
+                        controller: _passwordController,
+                        onChanged: (val) => password = val,
+                        obscureText: _obscureText,
+                        autofillHints: const [AutofillHints.password],
+                        decoration: InputDecoration(
+                          labelText: 'password',
+                          labelStyle: TextStyle(
+                            color: _passwordFocusNode.hasFocus
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(10, 15, 8, 20),
+                          border: CustomWidgetBorder(
+                              color: Colors.grey, width: 1.2),
+                          enabledBorder: CustomWidgetBorder(
+                              color: Colors.grey, width: 1.2),
+                          errorBorder: CustomWidgetBorder(
+                              color: Colors.red[300], width: 1.5),
+                          focusedErrorBorder: CustomWidgetBorder(
+                              color: Colors.red[300], width: 2.4),
+                          errorStyle: const TextStyle(fontSize: 14),
+                          focusedBorder: CustomWidgetBorder(
+                              color: Colors.blue, width: 2.2),
+                          prefixIcon: Icon(
+                            Icons.lock_rounded,
+                            color: _passwordFocusNode.hasFocus
+                                ? Colors.blue
+                                : Colors.grey,
+                          ),
+                          suffixIcon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              IconButton(
+                                icon: Icon(
+                                  _obscureText
+                                      ? Icons.visibility_rounded
+                                      : Icons.visibility_off_rounded,
+                                  color: _passwordFocusNode.hasFocus
+                                      ? Colors.blue
+                                      : Colors.grey,
+                                ),
+                                onPressed: () => setState(
+                                    () => _obscureText = !_obscureText),
                               ),
-                              onPressed: () =>
-                                  setState(() => _obscureText = !_obscureText),
-                            ),
-                            IconButton(
-                              icon: Icon(
-                                Icons.clear,
-                                color: _passwordFocusNode.hasFocus
-                                    ? Colors.blue
-                                    : Colors.grey,
+                              IconButton(
+                                icon: Icon(
+                                  Icons.clear,
+                                  color: _passwordFocusNode.hasFocus
+                                      ? Colors.blue
+                                      : Colors.grey,
+                                ),
+                                onPressed: () {
+                                  _passwordController.clear();
+                                  password = '';
+                                },
                               ),
-                              onPressed: () {
-                                _passwordController.clear();
-                                password = '';
-                              },
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
               const SizedBox(height: 20),
@@ -218,6 +226,8 @@ class _AdminLoginState extends State<AdminLogin> {
                   ElevatedButton(
                     child: const Text('Log In'),
                     onPressed: () async {
+                      TextInput.finishAutofillContext();
+
                       AuthService _auth = AuthService();
                       dynamic result =
                           await _auth.logIn(email: email, password: password);
