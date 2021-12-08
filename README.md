a3f118af580a33d4cd3177654a81a4269bd3a2bb
Hady Mashhour <63734405+HadyMash@users.noreply.github.com>
2021-12-09 00:32:33 +0400

    deleted everything

--- README.md
+++
@@ -1,3 +0,0 @@
-this was made for an ib pyschology experiment.
-
-Please note this is very very buggy and was made in a rush.
--- lib/shared/unlock_request_data.dart
+++
@@ -1,9 +0,0 @@
-class UnlockRequestData {
-  final String? docID;
-  final String uid;
-  final String? reason;
-  final bool? unlock;
-
-  const UnlockRequestData(
-      {this.docID, required this.uid, this.reason, this.unlock});
-}
